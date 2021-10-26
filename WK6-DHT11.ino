2e9a94ecb59176bd6772fb4850a655f87b95afab
mcqueen7 <83740852+mcqueen7@users.noreply.github.com>
2021-10-27 00:20:20 +0800

    Add files via upload

---
+++ WK6-DHT11.ino
@@ -0,0 +1,26 @@
+#include "DHT.h"  //引入函式庫
+#define DHT11PIN 14
+
+DHT dht(DHT11PIN, DHT11); //宣告DHT物件
+
+void setup()
+{
+  Serial.begin(115200);
+  dht.begin();  //DHT物件啟動
+  pinMode(12,OUTPUT);
+}
+
+void loop()
+{
+  float humi = dht.readHumidity();  //DHT讀取濕度
+  float temp = dht.readTemperature(); //DHT讀取溫度
+  
+  Serial.print("Temperature: ");
+  Serial.print(temp);
+  Serial.print("ºC ");
+  Serial.print("Humidity: ");
+  Serial.println(humi);
+  if(temp>=30)digitalWrite(12,HIGH);
+  else digitalWrite(12,LOW);
+  
+}
